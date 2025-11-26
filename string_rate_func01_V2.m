d62d8aac4bf185bb1e85140c2ba28a8f73c71166
lodio <lodio@olin.edu>
2025-11-26 13:51:24 -0800

    Simulation progress

---
+++ string_rate_func01_V2.m
@@ -0,0 +1,62 @@
+
+
+% INPUTS
+%   t: current time
+%   V: system state. V = [U; dUdt] where U and dUdt are nx1 column vectors
+%   string_params: a struct containing the system parameters describing the string
+%       string_params.n: number of masses
+%       string_params.M: total mass attached to the string
+%       string_params.Uf_func: function describing motion of end point
+%       string_params.dUfdt_func: time derivative of Uf
+%       string_params.Tf: tension in string
+%       string_params.L: length of string
+%       string_params.c: damping coefficient
+%       string_params.dx: horizontal spacing between masses
+% OUTPUTS
+%   dVdt: time derivative of the state vector V at time t, V = [dUdt; d2Udt2]
+%           has size nx1 column vector
+
+function dVdt = string_rate_func01_V2(t, V, string_params)
+    
+    % unpack string_params
+    n = string_params.n; % number of masses
+    M_total = string_params.M; % total mass attached to the string
+    Uf_func = string_params.Uf_func; % function describing motion of end point
+    dUfdt_func = string_params.dUfdt_func; % time derivative of Uf
+    Tf = string_params.Tf; % tension in string
+    c = string_params.c; % damping coefficient
+    dx = string_params.dx; % horizontal spacing between masses
+    
+    u0 = 0; % by sys definition
+    
+    % unpack state variable
+    U = V(1:n);
+    dUdt = V((n+1):(2*n));
+    
+    % compute acceleration
+    % M_total/n * u'' = Tf/dx * (u_i-1 - 2*u_i + u_i+1)     % normal sys
+    %                  + c/dx * (u_i-1' - 2*u_i' + u_i+1')  % + damping
+    % [M]*[u''] = Tf/dx*[K]*[u] + Tf/dx*[B_u]               % matrix vers
+    %           + c/dx*[K]*[u'] + c/dx*[B_u']
+    
+    % construct M
+    mi = M_total/n; I = eye(n); 
+    M = mi*I;
+    
+    % construct K
+    K = -2*I + circshift(I, [1, 0]) + circshift(I, [0, 1]);
+    K(end, 1) = 0; K(1, end) = 0;
+
+    % construct B (end conditions)
+    B_u = zeros(n, 1); B_dudt = zeros(n, 1);
+    B_u(1) = u0; B_u(end) = Uf_func(t);
+    B_dudt(1) = u0; B_dudt(end) = dUfdt_func(t);
+
+    % calculate 
+    % d2Udt2 = (M\K)*U;
+    Mud2 = (Tf/dx)*K*U + (Tf/dx)*B_u + (c/dx)*K*dUdt + (c/dx)*B_dudt;
+    d2Udt2 = M\Mud2;
+    
+    % assemble state derivative
+    dVdt = [dUdt; d2Udt2];
+end
---
+++ string_simulation.m
@@ -0,0 +1,47 @@
+%% simulate string and weights system
+
+function string_simulation()
+    
+    % assign system params
+    num_masses = 3; % per later analysis
+    total_mass = 4.5; % arbitrary, may need to change
+    tension_force = 5; % arbitrary, may need to change
+    string_length = 5; % selected arbitrarily
+    damping_coeff = 0.001; % selected per Orion's suggestion 0.1-0.01
+    dx = string_length/(num_masses+1);
+    amplitude_Uf = 0.1; % CHOSEN
+    omega_Uf = 0.3; % CHOSEN
+    
+    % construct the forcing function and its derivative:
+    % uf = A*cos(w*t), uf' = -w*A*sin(w*t)
+    Uf_func = @(t_in) amplitude_Uf*cos(omega_Uf*t_in);
+    dUfdt_func = @(t_in) -omega_Uf*amplitude_Uf*sin(omega_Uf*t_in);
+    
+    % generate the struct
+    string_params = struct();
+    string_params.n = num_masses;
+    string_params.M = total_mass;
+    string_params.Uf_func = Uf_func;
+    string_params.dUfdt_func = dUfdt_func;
+    string_params.Tf = tension_force;
+    string_params.L = string_length;
+    string_params.c = damping_coeff;
+    string_params.dx = dx;
+    
+    % load string_params into rate function
+    my_rate_func = @(t_in, V_in) string_rate_func01_V2(t_in, V_in, string_params);
+    
+    % initial conditions
+    U0 = 0.01*ones(num_masses, 1); % CHOSEN
+    dUdt0 = zeros(num_masses, 1); % CHOSEN
+    V0 = [U0; dUdt0];
+    tspan = [0, 15]; % CHOSEN
+    
+    % run the integration
+    [tlist, Vlist] = ode45(my_rate_func, tspan, V0); % may want to specify abstol?
+    
+    % animate the system
+    save_vid = false;
+    animate_string_sys(Vlist, tlist, string_params, save_vid)
+end
+
