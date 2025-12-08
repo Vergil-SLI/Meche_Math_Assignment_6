067d897d137d18751167b5d90e008577cfea0ba6
lodio <lodio@olin.edu>
2025-12-07 20:38:53 -0500

    Mostly complete discrete mode analysis.
    Individual simulations work. Pending an animate function that can animate both the string and the mode at the same time.

---
+++ simulate_mode_soln.m
@@ -0,0 +1,30 @@
+%% Simulate the string system via differential equations for a specified
+% environment / input. Here, the simulate code is refactored so that
+% the user can simply input 
+% each mass moves acoording to a sine fucntion**
+
+function simulate_mode_soln(U_r, omega, damping, tspan, string_params, save_vid)
+    
+    % unpack string_params
+    num_masses = string_params.n;
+    string_params.c = damping; % set damping coeff
+    
+    % solution has form
+    % U(t) = U_r*cos(ω_r*t + φ)  
+
+    string_params.Uf_func = @(t_in) 0*t_in;
+    string_params.dUfdt_func = @(t_in) 0*t_in;
+
+    % generate a tlist
+    tlist = linspace(tspan(1), tspan(2), 100*diff(tspan));
+    
+    % construct Vlist (according to the ideal cosine movement)
+    Vlist = zeros(length(tlist), num_masses);
+    for i = 1:num_masses
+        Vlist(:,i) = U_r(i)*cos(omega*tlist - pi/2);
+    end
+
+    % animate result
+    animate_string(Vlist, tlist, string_params, save_vid)
+
+end
